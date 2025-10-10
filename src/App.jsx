dbda1739c479313f7bbb4498d7d67c1d655d7609
Divya John <divelzjon@gmail.com>
2025-10-10 09:31:48 +0530

    TaskForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import TaskForm from './components/TaskForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className='min-h-screen bg-gra-100 p-6'>
+      <h1 className='text-2xl font-bold text-center mb-6'>Task Manager</h1>
+      <TaskForm/>
+    </div>
   );
 }
 
