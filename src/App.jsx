90047a25265ca1ffb645a6f3367b595f2f7d3327
Nandanarayaroth <nandanarayaroth7@gmail.com>
2025-10-10 09:41:17 +0530

    status and filter page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Status from './components/Status';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Status/>
+      
     </>
   );
 }
---
+++ src/components/Filter.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+function Filter() {
+
+    const [selectedPriority, Setpriority] = useState("All")
+
+    const handleChangefilter = (e) => {
+        Setpriority(e.target.value)
+    }
+
+    return(
+        <div className="border border-gray-300">
+            <h3 className="text-2xl m-2">Filter</h3>
+            <input
+                className="m-4"
+            type="radio" value={"All"} name="filter" checked={selectedPriority === "All"} onChange={handleChangefilter}/><label>All</label>
+            <input
+                className="m-4"
+            type="radio" value={"Low"} name="filter" checked={selectedPriority === "Low"} onChange={handleChangefilter}/> <label>Low</label>
+            <input 
+                className="m-4"
+            type="radio" value={"Medium"} name="filter" checked={selectedPriority === "Medium"} onChange={handleChangefilter}/> <label>/Medium</label>
+            <input 
+                className="m-4"
+            type="radio" value={"High"} name="filter" checked={selectedPriority === "High"} onChange={handleChangefilter}/><label>High</label>
+
+            <div>{selectedPriority}</div>
+        </div>
+    )
+}
+
+export default Filter 
